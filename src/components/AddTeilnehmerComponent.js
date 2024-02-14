366f838067b40ffb66fc18653edbc4de9df32a93
KenanSalar <kenansalar@outlook.com>
2024-02-14 14:55:33 +0100

    teilnehmer update

---
+++ src/components/AddTeilnehmerComponent.js
@@ -0,0 +1,202 @@
+import React, { useState, useEffect } from "react";
+import TeilnehmerService from "../services/TeilnehmerService";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+const AddTeilnehmerComponent = () => {
+  const [vorname, setVorname] = useState("");
+  const [nachname, setNachname] = useState("");
+  const [position, setPosition] = useState("");
+  const [phone, setTelefon] = useState("");
+  const [plz, setPlz] = useState("");
+  const [ort, setOrt] = useState("");
+  const [strasse, setStrasse] = useState("");
+  const [hause_nr, setHausNummer] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const teilnehmerData = {
+    vorname,
+    nachname,
+    position,
+    phone,
+    plz,
+    ort,
+    strasse,
+    email,
+    hause_nr,
+  };
+
+  useEffect(() => {
+    if (id) {
+      TeilnehmerService.getTeilnehmerById(id)
+        .then((res) => {
+          const {
+            teilnehmer_vorname,
+            teilnehmer_nachname,
+            teilnehmer_position,
+          } = res.data.teilnehmer[0];
+          const {
+            kd_email,
+            kd_haus_nr,
+            kd_ort,
+            kd_plz,
+            kd_straße,
+            kd_phone_nr,
+          } = res.data.kontaktDaten[0];
+          setVorname(teilnehmer_vorname);
+          setNachname(teilnehmer_nachname);
+          setPosition(teilnehmer_position);
+          setTelefon(kd_phone_nr);
+          setPlz(kd_plz);
+          setOrt(kd_ort);
+          setStrasse(kd_straße);
+          setHausNummer(kd_haus_nr);
+          setEmail(kd_email);
+        })
+        .catch((e) => console.log(e));
+    }
+  }, []);
+
+  // senden data zu api und navigate wenn alles gut
+  function speicherTeilnehmer(e) {
+    e.preventDefault();
+
+    if (
+      teilnehmerData.vorname !== "" &&
+      teilnehmerData.nachname !== "" &&
+      teilnehmerData.email != ""
+    ) {
+      /**If id is present in the parameter, it should update else it should save */
+      if (id) {
+        TeilnehmerService.updateTeilnehmer(id, teilnehmerData)
+          .then(navigate("/teilnehmer"))
+          .catch((e) => console.log(e));
+      } else {
+        TeilnehmerService.speicherTeilnehmer(teilnehmerData)
+          .then(navigate("/teilnehmer"))
+          .catch((e) => console.log(e));
+      }
+    } else {
+      alert("Please, fill in all inputes");
+    }
+  }
+
+  //Update oder Add Titel
+
+  function title() {
+    if (id) {
+      return "Teilnehmer bearbeiten";
+    } else {
+      return "Teilnehmer hinzufügen";
+    }
+  }
+
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="card col-md-6 offset-md-3">
+            <h2 className="text-center">{title()}</h2>
+            <div className="card-body">
+              <form>
+                <div className="form-group mb-2">
+                  <input
+                    className="form-control"
+                    value={vorname}
+                    onChange={(e) => setVorname(e.target.value)}
+                    type="text"
+                    placeholder="Vorname"
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    className="form-control"
+                    value={nachname}
+                    onChange={(e) => setNachname(e.target.value)}
+                    type="text"
+                    placeholder="Nachname"
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    className="form-control"
+                    value={position}
+                    onChange={(e) => setPosition(e.target.value)}
+                    type="text"
+                    placeholder="Position"
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    className="form-control"
+                    value={phone}
+                    onChange={(e) => setTelefon(e.target.value)}
+                    type="text"
+                    placeholder="Telefon"
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    className="form-control"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    className="form-control"
+                    value={plz}
+                    onChange={(e) => setPlz(e.target.value)}
+                    type="text"
+                    placeholder="Plz"
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    className="form-control"
+                    value={ort}
+                    onChange={(e) => setOrt(e.target.value)}
+                    type="text"
+                    placeholder="Ort"
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    className="form-control"
+                    value={strasse}
+                    onChange={(e) => setStrasse(e.target.value)}
+                    type="text"
+                    placeholder="Strasse"
+                  />
+                </div>
+                <div className="form-group mb-2">
+                  <input
+                    className="form-control"
+                    value={hause_nr}
+                    onChange={(e) => setHausNummer(e.target.value)}
+                    type="text"
+                    placeholder="Haus Nummer"
+                  />
+                </div>
+                <button
+                  onClick={(e) => speicherTeilnehmer(e)}
+                  className="btn btn-success"
+                >
+                  Speichern
+                </button>{" "}
+                <Link to={"/teilnehmer"} className="btn btn-danger" href="">
+                  Stornieren
+                </Link>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddTeilnehmerComponent;
---
+++ src/components/ListTeilnehmerComponent.js
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import TeilnehmerService from "../services/TeilnehmerService";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import SearchIcon from "@mui/icons-material/Search";
+
+const ListTeilnehmerComponent = () => {
+  const [TeilnehmerArray, setTeilnehmerArray] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    getTeilnehmer();
+  }, []);
+
+  function getTeilnehmer() {
+    TeilnehmerService.getTeilnehmer()
+      .then((res) => {
+        setTeilnehmerArray(res.data);
+      })
+      .catch((e) => console.log(e));
+  }
+
+  function deleteTeilnehmer(e, id) {
+    e.preventDefault();
+    console.log(id);
+    TeilnehmerService.deleteTeilnehmer(id)
+      .then(window.location.reload(true))
+      .catch((e) => console.log(e));
+  }
+
+  return (
+    <div className="container">
+      <div className="input-group mb-3">
+        <input
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          type="text"
+          className="form-control"
+          placeholder="Suchen nach Nachname"
+          aria-label="Suchen"
+          aria-describedby="basic-addon2"
+        />
+        <div className="input-group-append">
+          <span className="input-group-text" id="basic-addon2">
+            <SearchIcon />
+          </span>
+        </div>
+      </div>
+
+      <h2 className="text-center mb-4">List Teilnehmer</h2>
+      <table className="table table-bordered table striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Vorname</th>
+            <th>Nachname</th>
+            <th>Email</th>
+            <th>Telefon</th>
+            <th>Adresse</th>
+            <th>Position</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {TeilnehmerArray.filter((item) => {
+            return search.toLowerCase() === ""
+              ? item
+              : item.teilnehmer_nachname.toLowerCase().includes(search);
+          }).map((teilnehmer) => (
+            <tr key={teilnehmer.teilnehmer_id} id={teilnehmer.teilnehmer_id}>
+              <td>{teilnehmer.teilnehmer_id}</td>
+              <td>{teilnehmer.teilnehmer_vorname}</td>
+              <td>{teilnehmer.teilnehmer_nachname}</td>
+              <td>{teilnehmer.kd_email}</td>
+              <td>{teilnehmer.kd_phone_nr}</td>
+              <td>
+                {teilnehmer.kd_ort} {teilnehmer.kd_plz} {teilnehmer.kd_straße}{" "}
+                {teilnehmer.kd_haus_nr}
+              </td>
+              <td>{teilnehmer.teilnehmer_position}</td>
+
+              <td>
+                <Link
+                  to={`/add-teilnehmer/${teilnehmer.teilnehmer_id}`}
+                  className="btn btn-info action"
+                >
+                  <EditIcon />
+                </Link>
+                <a
+                  onClick={(e) => {
+                    deleteTeilnehmer(e, teilnehmer.teilnehmer_id);
+                  }}
+                  className="btn btn-danger"
+                  href=""
+                >
+                  <DeleteIcon />
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Link
+        to={"/add-teilnehmer"}
+        className="btn btn-primary mb-2 mt-3"
+        href=""
+      >
+        Add Teilnehmer
+      </Link>
+    </div>
+  );
+};
+
+export default ListTeilnehmerComponent;
