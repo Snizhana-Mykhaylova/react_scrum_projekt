95b0537d8d6c305df8f8edea93ef58b2c391d5bf
Yakup Budak <y.budak@mailbox.org>
2024-02-20 11:23:01 +0100

    footer und homepage update

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 import buchImg from "../img/buch.jpg";
+import csharpImg from "../img/cis.png";
+import cppImg from "../img/cpp.png";
+import sqlImg from "../img/SQL.png";
 import workspacebackgroundImg from "../img/workspacebackground.png";
 import workspaceImg from "../img/workspace.jpg";
 
@@ -9,10 +12,10 @@ const MainComponent = () => {
     <main className="container">
       <section className="baner">
         <div className="banerText">
-          <h2>Die COMKOLLEGE verwaltungs web-App</h2>
+          <h2>Die ComKollege verwaltungs web-App</h2>
           <p>
-            Mit unserer Innovativen web app ermöglichen wir dem anwender eine
-            übersichtliche verwaltung seiner Vakanzen, sowie die erstellung von
+            Mit unserer Innovativen web app ermöglichen wir dem Anwender eine
+            übersichtliche Verwaltung seiner Vakanzen, sowie die erstellung von
             Kurs Modulen mit der möglichkeit Dozenten, Teilnehmer und
             Mitarbeiter zuzuweisen.
           </p>
@@ -25,58 +28,52 @@ const MainComponent = () => {
         />
       </section>
 
-      <section className="beschreibung">
-        <p>Beschreibung</p>
-        <h2>Flexible einsatzmöglichkeiten</h2>
-        <p>
-          Egal ob zuhause am Computer, im garten am Tablet oder unterwegs über
-          das Smartphone, unsere webapp lässt sich auf nahezu allen
-          betriebssystemen die einen Webbrowser öffnen können, verwenden. Der
-          anwender hat die möglichkeit von "überall" aus, seine Firma mit
-          leichtigkeit zu verwalten.{" "}
-        </p>
+      <section className="baner2">
+        <div className="beschreibung">
+          <h2>Flexible einsatzmöglichkeiten</h2>
+
+          <p>
+            Egal ob zuhause am Computer, im Garten am Tablet oder unterwegs über
+            das Smartphone, unsere webapp lässt sich auf nahezu allen
+            betriebssystemen die einen Webbrowser öffnen können, verwenden. Der
+            Anwender hat die möglichkeit von "überall" aus, sein Unternehmen mit
+            leichtigkeit zu verwalten.{" "}
+          </p>
+        </div>
         <img className="workspace" src={workspaceImg} alt="" />
       </section>
-
       <section>
-        <ul className="kursCardList ">
-          <li className="kursCard">
-            <div className="card">
-              <img src={buchImg} className="card-img-top" alt="kurs1" />
-              <div className="card-body">
-                <h5 className="card-title">Kurse</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
+        {
+          <ul className="kursCardList ">
+            <li className="kursCard">
+              <div className="card">
+                <img src={sqlImg} className="card-img-top" alt="kurs1" />
+                <div className="card-body">
+                  <h5 className="card-title">SQL</h5>
+                  <p className="card-text">Datenbanken, Listen, Tabellen</p>
+                </div>
               </div>
-            </div>
-          </li>
-          <li className="kursCard">
-            <div className="card">
-              <img src={buchImg} className="card-img-top" alt="kurs2" />
-              <div className="card-body">
-                <h5 className="card-title">Klassen</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
+            </li>
+            <li className="kursCard">
+              <div className="card">
+                <img src={csharpImg} className="card-img-top" alt="kurs2" />
+                <div className="card-body">
+                  <h5 className="card-title">C#</h5>
+                  <p className="card-text">Objektorientierte Programmierung</p>
+                </div>
               </div>
-            </div>
-          </li>
-          <li className="kursCard">
-            <div className="card">
-              <img src={buchImg} className="card-img-top" alt="kurs3" />
-              <div className="card-body">
-                <h5 className="card-title">Teilnehmer</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
+            </li>
+            <li className="kursCard">
+              <div className="card">
+                <img src={cppImg} className="card-img-top" alt="kurs3" />
+                <div className="card-body">
+                  <h5 className="card-title">C++</h5>
+                  <p className="card-text">Grundlagen der Programmierung.</p>
+                </div>
               </div>
-            </div>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        }
       </section>
     </main>
   );
