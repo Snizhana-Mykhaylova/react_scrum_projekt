ff5f3ee65e54ea4f745ca2bda2e417a026c41ddb
Yakup Budak <y.budak@mailbox.org>
2024-02-20 15:26:16 +0100

    carousel

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,15 +1,61 @@
 import React from "react";
 
-import buchImg from "../img/buch.jpg";
 import csharpImg from "../img/cis.png";
 import cppImg from "../img/cpp.png";
 import sqlImg from "../img/SQL.png";
 import workspacebackgroundImg from "../img/workspacebackground.png";
 import workspaceImg from "../img/workspace.jpg";
 
+import Carousel from 'react-bootstrap/Carousel';
+// import ExampleCarouselImage from 'components/ExampleCarouselImage';
+
 const MainComponent = () => {
   return (
     <main className="container">
+            <section className="baner2">
+        <div className="beschreibung">
+          <h2>Flexible einsatzmöglichkeiten</h2>
+
+          <p>
+            Egal ob zuhause am Computer, im Garten am Tablet oder unterwegs über
+            das Smartphone, unsere webapp lässt sich auf nahezu allen
+            betriebssystemen die einen Webbrowser öffnen können, verwenden. Der
+            Anwender hat die möglichkeit von "überall" aus, sein Unternehmen mit
+            leichtigkeit zu verwalten.{" "}
+          </p>
+        </div>
+        <img className="workspace" src={workspaceImg} alt="" />
+      </section>
+      <section>
+    <Carousel className="carousel_item">
+      <Carousel.Item >
+      <img src={sqlImg} className="card-img-top" alt="kurs1" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item >
+      <img src={csharpImg} className="card-img-top" alt="kurs2" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item >
+      
+      <Carousel.Item >
+      <img src={cppImg} className="card-img-top" alt="kurs3" />
+        <Carousel.Caption> 
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+       
+      </section>
       <section className="baner">
         <div className="banerText">
           <h2>Die ComKollege verwaltungs web-App</h2>
@@ -28,53 +74,7 @@ const MainComponent = () => {
         />
       </section>
 
-      <section className="baner2">
-        <div className="beschreibung">
-          <h2>Flexible einsatzmöglichkeiten</h2>
 
-          <p>
-            Egal ob zuhause am Computer, im Garten am Tablet oder unterwegs über
-            das Smartphone, unsere webapp lässt sich auf nahezu allen
-            betriebssystemen die einen Webbrowser öffnen können, verwenden. Der
-            Anwender hat die möglichkeit von "überall" aus, sein Unternehmen mit
-            leichtigkeit zu verwalten.{" "}
-          </p>
-        </div>
-        <img className="workspace" src={workspaceImg} alt="" />
-      </section>
-      <section>
-        {
-          <ul className="kursCardList ">
-            <li className="kursCard">
-              <div className="card">
-                <img src={sqlImg} className="card-img-top" alt="kurs1" />
-                <div className="card-body">
-                  <h5 className="card-title">SQL</h5>
-                  <p className="card-text">Datenbanken, Listen, Tabellen</p>
-                </div>
-              </div>
-            </li>
-            <li className="kursCard">
-              <div className="card">
-                <img src={csharpImg} className="card-img-top" alt="kurs2" />
-                <div className="card-body">
-                  <h5 className="card-title">C#</h5>
-                  <p className="card-text">Objektorientierte Programmierung</p>
-                </div>
-              </div>
-            </li>
-            <li className="kursCard">
-              <div className="card">
-                <img src={cppImg} className="card-img-top" alt="kurs3" />
-                <div className="card-body">
-                  <h5 className="card-title">C++</h5>
-                  <p className="card-text">Grundlagen der Programmierung.</p>
-                </div>
-              </div>
-            </li>
-          </ul>
-        }
-      </section>
     </main>
   );
 };
